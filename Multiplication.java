100cd620cf180c1c8b1af4cc7ea02ef5d680e843
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-11 10:50:32 +0530

    import java.util.Scanner; public class bhanu{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         int m;         m=a*b;         System.out.print(m);     }
    }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class bhanu{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int m;
+        m=a*b;
+        System.out.print(m);
+    }
+    
+}
