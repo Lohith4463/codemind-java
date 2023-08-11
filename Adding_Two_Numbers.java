91105cb4b51f4e13ee53ab6a6545f6cdf792b461
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-11 11:05:53 +0530

    import java.util.Scanner; public class harsha{     public static void main(String[] args){         Scanner h=new Scanner(System.in);         int a=h.nextInt();         int b=h.nextInt();         System.out.print(a+b);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class harsha{
+    public static void main(String[] args){
+        Scanner h=new Scanner(System.in);
+        int a=h.nextInt();
+        int b=h.nextInt();
+        System.out.print(a+b);
+    }
+}
