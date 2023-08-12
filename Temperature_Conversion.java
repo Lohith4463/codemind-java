e41b1ce24a0b6ac387b48fddf3e06cb60f9401e8
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-12 11:19:47 +0530

    import java.util.Scanner;     public class harsha{         public static void main(String [] args){             Scanner sc=new Scanner(System.in);             float t=sc.nextFloat();             double tc;             tc=32+(t*9/5);             System.out.printf("%.2f",tc);         } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+    public class harsha{
+        public static void main(String [] args){
+            Scanner sc=new Scanner(System.in);
+            float t=sc.nextFloat();
+            double tc;
+            tc=32+(t*9/5);
+            System.out.printf("%.2f",tc);
+        }
+}
