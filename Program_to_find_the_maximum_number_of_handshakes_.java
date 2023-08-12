c9cab5171744bfcdc7c3b79a693da7eb3d39e453
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-12 12:07:53 +0530

    import java.util.Scanner; public class bhanu{     public static void main(String [] args){         Scanner s=new Scanner(System.in);         int a,b;         a=s.nextInt();         b=a*(a-1)/2;         System.out.print(b);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class bhanu{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        int a,b;
+        a=s.nextInt();
+        b=a*(a-1)/2;
+        System.out.print(b);
+    }
+}
