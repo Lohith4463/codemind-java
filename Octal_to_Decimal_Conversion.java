9d1523f19794435cf53fcefe57554fba50767d38
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-19 06:50:51 +0530

    import java.util.*; public class lohith{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s;         s=sc.next();         int n=Integer.parseInt(s,8);         System.out.print(n);
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class lohith{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.next();
+        int n=Integer.parseInt(s,8);
+        System.out.print(n);
+        
+    }
+}
