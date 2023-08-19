1eb94b165e30fbc53a0b043e51660983a340656c
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-19 09:58:53 +0530

    import java.util.*; public class harstar{     public static void main(String [] args){         Scanner s=new Scanner(System.in);         int n,i,j;         n=s.nextInt();         if(n>=3 && n<=100){             for(i=1;i<=n;i++){                 for(j=1;j<=i;j++)                     System.out.print("*");                     System.out.print(" ");             }                 for(i=1;i<=n-1;i++){                     for(j=i;j<=n-1;j++)                         System.out.print("*");                         System.out.print(" ");                 }         }         else{             System.out.print("The pattern is not possible");         }
        }
    }

---
+++ Half_diamond_star_pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class harstar{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        int n,i,j;
+        n=s.nextInt();
+        if(n>=3 && n<=100){
+            for(i=1;i<=n;i++){
+                for(j=1;j<=i;j++)
+                    System.out.print("*");
+                    System.out.print("
+");
+            }
+                for(i=1;i<=n-1;i++){
+                    for(j=i;j<=n-1;j++)
+                        System.out.print("*");
+                        System.out.print("
+");
+                }
+        }
+        else{
+            System.out.print("The pattern is not possible");
+        }
+        
+    }
+}
