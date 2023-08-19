3bc828f6b073d9333d49f7790a45cc157fe6f88e
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-19 07:08:53 +0530

    import java.util.*; public class loh{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int s,t,b;         s=sc.nextInt();         t=sc.nextInt();         b=sc.nextInt();         System.out.println(s*t*b*512*2);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class loh{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int s,t,b;
+        s=sc.nextInt();
+        t=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(s*t*b*512*2);
+    }
+}
