20234b82ddf2dedf9f2be5339321f45f71af7e49
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-22 22:23:45 +0530

    import java.util.*; public class mai{     public static void main(String [] args){         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=1;i<=n;i++){             for(j=1;j<=n;j++){                 System.out.print(i+ " ");             }             System.out.print(" ");         }     } }

---
+++ print_the_following_pattern_16.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class mai{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        n=sc.nextInt();
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+                System.out.print(i+ " ");
+            }
+            System.out.print("
+");
+        }
+    }
+}
