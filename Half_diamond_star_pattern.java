a8b892ff7986892940e8428805072db34afcd46d
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-24 06:46:17 +0530

    import java.util.Scanner; public class A{     public static void main(String[] args)     {          Scanner sc=new Scanner(System.in);          int n=sc.nextInt();          if(n>=3&&n<=100)          {              for(int i=1;i<=n;i++)              {                  for(int j=1;j<=i;j++)                  System.out.print("*");                  System.out.print(" ");              }              for(int i=1;i<=n-1;i++)              {                  for(int j=i;j<=n-1;j++)                  System.out.print("*");                  System.out.print(" ");              }          }          else          {              System.out.print("The pattern is not possible");          }     } }

--- Half_diamond_star_pattern.java
+++ Half_diamond_star_pattern.java
@@ -1,26 +1,29 @@
-import java.util.*;
-public class harstar{
-    public static void main(String [] args){
-        Scanner s=new Scanner(System.in);
-        int n,i,j;
-        n=s.nextInt();
-        if(n>=3 && n<=100){
-            for(i=1;i<=n;i++){
-                for(j=1;j<=i;j++)
-                    System.out.print("*");
-                    System.out.print("
+import java.util.Scanner;
+public class A{
+    public static void main(String[] args)
+    {
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         if(n>=3&&n<=100)
+         {
+             for(int i=1;i<=n;i++)
+             {
+                 for(int j=1;j<=i;j++)
+                 System.out.print("*");
+                 System.out.print("
 ");
-            }
-                for(i=1;i<=n-1;i++){
-                    for(j=i;j<=n-1;j++)
-                        System.out.print("*");
-                        System.out.print("
+             }
+             for(int i=1;i<=n-1;i++)
+             {
+                 for(int j=i;j<=n-1;j++)
+                 System.out.print("*");
+                 System.out.print("
 ");
-                }
-        }
-        else{
-            System.out.print("The pattern is not possible");
-        }
-        
+             }
+         }
+         else
+         {
+             System.out.print("The pattern is not possible");
+         }
     }
 }
